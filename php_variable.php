0e51ad0125e5630924b111d5f9b7e189c07a4d9e
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2024-12-20 10:26:47 +0600

    variable type

---
+++ php_variable.php
@@ -0,0 +1,9 @@
+<?php 
+echo "variable are int float double boolean";
+$name ="nahid";
+$age="26";
+$hobbies=['playing','learning','teaching','income'];
+$IsSingle=false;
+echo"details are", $name,$age,$hobbies,$IsSingle;
+print $name;
+?>
