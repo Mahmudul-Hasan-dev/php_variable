49d61d023eb1239692b84040abbcabefead70cf1
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2024-12-20 10:29:01 +0600

    comment added print vs echo

--- php_variable.php
+++ php_variable.php
@@ -4,6 +4,6 @@ $name ="nahid";
 $age="26";
 $hobbies=['playing','learning','teaching','income'];
 $IsSingle=false;
-echo"details are", $name,$age,$hobbies,$IsSingle;
-print $name;
+echo"details are", $name,$age,$hobbies,$IsSingle; //echo for every printing
+print $name;//print will be used for just returning or printing variable
 ?>
